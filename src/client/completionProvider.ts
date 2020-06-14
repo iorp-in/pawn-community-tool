7fa5fb325696b90e622c2525dfc3c77d12861d63
IORP <indianoceanroleplay@gmail.com>
2020-06-14 10:44:19 +0530

    new auto complete

---
+++ src/client/completionProvider.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode';
+import { exists, existsSync } from 'fs';
+
+let snippetCollection: Map<string, vscode.CompletionItem> = new Map();
+
+export const initSnippetCollector = async () => {
+    snippetCollection.clear();
+    const files = await vscode.workspace.findFiles('**/*.pwn');
+    for (const key in files) {
+        if (files.hasOwnProperty(key)) {
+            const element = files[key];
+            const content = (await vscode.workspace.openTextDocument(element)).getText();
+            parseSnippets(content);
+        }
+    }
+    const filesInc = await vscode.workspace.findFiles('**/*.inc');
+    for (const key in filesInc) {
+        if (filesInc.hasOwnProperty(key)) {
+            const element = filesInc[key];
+            const content = (await vscode.workspace.openTextDocument(element)).getText();
+            parseSnippets(content);
+        }
+    }
+};
+
+const parseSnippets = (content: string) => {
+    const regex = /(forward|native|stock)\s(.*?)\((.*?)\)/gm;
+    var m;
+    do {
+        m = regex.exec(content);
+        if (m) {
+            const newSnip = new vscode.CompletionItem(m[2]);
+            newSnip.insertText = m[2] + '(' + m[3] + ')';
+            newSnip.kind = vscode.CompletionItemKind.Function;
+            const findSnip = snippetCollection.get(m[2]);
+            if (findSnip === undefined) snippetCollection.set(m[2], newSnip);
+        }
+    } while (m);
+};
+
+const provideCompletion = {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+        return Array.from(snippetCollection.values());
+    }
+};
+
+export default provideCompletion;
--- src/client/extension.ts
+++ src/client/extension.ts
@@ -1,81 +1,15 @@
 import BuildTaskHandler from './buildTask';
 import PawnDocumentFormattingEditProvider from './formatter';
-import PawnHoverProvider from './HoverProvider';
-import {
-	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri, commands, languages, DocumentSelector, SnippetString
-} from 'vscode';
-import * as Path from 'path';
-import * as VSC from 'vscode';
-import * as VSCLC from 'vscode-languageclient';
-import * as Commands from './commands';
-let diagnosticCollection: VSC.DiagnosticCollection;
+import * as vscode from 'vscode';
+import provideCompletion, { initSnippetCollector } from './completionProvider';
 
-export function activate(context: ExtensionContext) {  // The server is implemented in node
-	context.subscriptions.push(commands.registerCommand('pawn-community-tool.initTask', BuildTaskHandler));
-	languages.registerDocumentFormattingEditProvider('pawn', PawnDocumentFormattingEditProvider);
-	languages.registerDocumentRangeFormattingEditProvider('pawn', PawnDocumentFormattingEditProvider);
-	//languages.registerHoverProvider('pawn', PawnHoverProvider);
-	const serverModulePath = context.asAbsolutePath(Path.join('out', 'server', 'server.js'));
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=5858'] };
-
-	const serverOptions: VSCLC.ServerOptions = {
-		run: {
-			module: serverModulePath,
-			transport: VSCLC.TransportKind.ipc,
-			options: debugOptions
-		},
-		debug: {
-			module: serverModulePath,
-			transport: VSCLC.TransportKind.ipc,
-			options: debugOptions
-		}
-	};
-
-	const clientOptions: VSCLC.LanguageClientOptions = {
-		documentSelector: ['pawn'],
-		synchronize: {
-			configurationSection: [
-				'pawn.language',
-				'pawn.compiler'
-			],
-			fileEvents: VSC.workspace.createFileSystemWatcher('**/*.*')
-		}
-	};
-
-	const languageClient = new VSCLC.LanguageClient('pawn', 'Pawn Language Service', serverOptions, clientOptions);
-
-	const outputChannel = VSC.window.createOutputChannel('PAWN Output');
-
-	diagnosticCollection = VSC.languages.createDiagnosticCollection('pawn');
-
-	const commandCompile = VSC.commands.registerCommand('pawn.compile', Commands.compile.bind(null, outputChannel, diagnosticCollection));
-	const commandCompileLocal = VSC.commands.registerCommand('pawn.compileLocal', Commands.compileLocal.bind(null, outputChannel, diagnosticCollection));
-
-	VSC.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
-
-	// Push all disposables
-	context.subscriptions.push(
-		languageClient.start(),
-
-		diagnosticCollection,
-
-		// Commands
-		commandCompile,
-		commandCompileLocal,
-
-		// Output channels
-		VSC.Disposable.from(outputChannel)
-	);
+export async function activate(context: vscode.ExtensionContext) {  // The server is implemented in node
+	context.subscriptions.push(vscode.commands.registerCommand('pawn-community-tool.initTask', BuildTaskHandler));
+	vscode.languages.registerDocumentFormattingEditProvider('pawn', PawnDocumentFormattingEditProvider);
+	vscode.languages.registerCompletionItemProvider('pawn', provideCompletion);
+	vscode.languages.registerDocumentRangeFormattingEditProvider('pawn', PawnDocumentFormattingEditProvider);
+	vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+		initSnippetCollector();
+	});
+	initSnippetCollector();
 }
-
-function onDidChangeTextDocument(ev: VSC.TextDocumentChangeEvent) {
-	diagnosticCollection.delete(ev.document.uri);
-}
-
-
-export function deactivate() {
-
-}
-
-
-
