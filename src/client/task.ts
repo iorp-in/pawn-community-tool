e4d680fff81d0ac9f2ed7e04fb8cb80e1fe60199
IORP <indianoceanroleplay@gmail.com>
2020-06-14 10:58:42 +0530

    buil task fix

--- src/client/task.ts
+++ src/client/task.ts
@@ -1,27 +1,43 @@
 export var task = `{
-\t"version": "2.0.0",
-\t"tasks": [
-\t\t{
-\t\t\t"label": "build-normal",
-\t\t\t"type": "shell",
-\t\t\t"command": "pawncc", // pawncc compiler location, we suggest to add pawncc path into your environment
-\t\t\t"args": [
-\t\t\t\t"grandlarc.pwn", // source file of gamemode
-\t\t\t\t"-Dgamemodes", // Additional compiler arguments  
-\t\t\t\t"-;+",
-\t\t\t\t"-(+",
-\t\t\t\t"-d3"
-\t\t\t],
-\t\t\t"group": {
-\t\t\t\t"kind": "build",
-\t\t\t\t"isDefault": true
-\t\t\t},
-\t\t\t"isBackground": false,
-\t\t\t"presentation": {
-\t\t\t\t"reveal": "silent",
-\t\t\t\t"panel": "dedicated"
-\t\t\t},
-\t\t\t"problemMatcher": "$pawncc"
-\t\t}
-\t]
+	"version": "2.0.0",
+	"tasks": [
+		{
+			"label": "build-normal",
+			"type": "shell",
+			"command": "pawncc",
+			"runOptions": {
+				"instanceLimit": 1,
+			},
+			"args": [
+				"ior.pwn",
+				"-Dgamemodes",
+				"-;+",
+				"-(+",
+				"-d3"
+			],
+			"group": {
+				"kind": "build",
+				"isDefault": true
+			},
+			"isBackground": false,
+			"presentation": {
+				"reveal": "always",
+				"panel": "dedicated"
+			},
+			"problemMatcher": {
+				"owner": "pawn",
+				"fileLocation": [
+					"relative",
+					"\${workspaceRoot}"
+				],
+				"pattern": {
+					"regexp": "^(.*?)\\\\(([0-9]*)[- 0-9]*\\\\) \\\\: (fatal error|error|warning) [0-9]*\\\\: (.*)$",
+					"file": 1,
+					"location": 2,
+					"severity": 3,
+					"message": 4
+				}
+			}
+		}
+	],
 }`;
