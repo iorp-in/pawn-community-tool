021cfa2b34e777c486b5315933099c87c9751ff9
IORP <indianoceanroleplay@gmail.com>
2021-03-06 01:11:11 +0530

    update

--- src/server/parser.ts
+++ src/server/parser.ts
@@ -19,7 +19,7 @@ export const resetAutocompletes = () => {
 };
 
 export const parseDefine = (textDocument: TextDocument) => {
-    const regexDefine = /^#define\s([A-Za-z_]*?)($|\s)/gm;
+    const regexDefine = /^#define\s+([^()]*?)\s+(.*?)$/gm;
     const content = textDocument.getText();
     const splitContent = content.split('\n');
     splitContent.forEach((cont: string, index: number) => {
@@ -197,7 +197,76 @@ export const parsefuncsWithoutPrefix = (textDocument: TextDocument) => {
                 });
                 let params: ParameterInformation[] = [];
                 if (m[2].trim().length > 0) {
-                    params = m[1].split(',').map((value) => ({ label: value.trim() }));
+                    params = m[2].split(',').map((value) => ({ label: value.trim() }));
+                } else {
+                    params = [];
+                }
+                const pwnFun: PawnFunction = {
+                    textDocument: textDocument,
+                    definition: newDef,
+                    completion: newSnip,
+                    params
+                };
+                const indexPos = m[1].indexOf(':');
+                if (indexPos !== -1) {
+                    const resOut = /:(.*)/gm.exec(m[1]);
+                    if (resOut) m[1] = resOut[1];
+                }
+                const findSnip = pawnFuncCollection.get(m[1]);
+                if (findSnip === undefined) pawnFuncCollection.set(m[1], pwnFun);
+            }
+        } while (m);
+    });
+};
+
+export const parsefuncsDefines = (textDocument: TextDocument) => {
+    const regex = /^#define\s+(\S*?)\((.*?)\)/gm;
+    const content = textDocument.getText();
+    const splitContent = content.split('\n');
+    splitContent.forEach((cont: string, index: number) => {
+        var m;
+        do {
+            m = regex.exec(cont);
+            if (m) {
+                let doc: string = '';
+                let endDoc = -1;
+                if (splitContent[index - 1] !== undefined) endDoc = splitContent[index - 1].indexOf('*/');
+                if (endDoc !== -1) {
+                    let startDoc = -1;
+                    let inNum = index;
+                    while (inNum >= 0) {
+                        inNum--;
+                        if (splitContent[inNum] === undefined) continue;
+                        startDoc = splitContent[inNum].indexOf('/*');
+                        if (startDoc !== -1) {
+                            if (inNum === index) {
+                                doc = splitContent[index];
+                            } else if (inNum < index) {
+                                while (inNum < index) {
+                                    doc += splitContent[inNum] + '\n\n';
+                                    inNum++;
+
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                doc = doc.replace('/*', '').replace('*/', '').trim();
+                const newSnip: CompletionItem = {
+                    label: m[1] + '(' + m[2] + ')',
+                    kind: CompletionItemKind.Function,
+                    insertText: m[1] + '(' + m[2] + ')',
+                    documentation: doc,
+                };
+                const newDef: Definition = Location.create(textDocument.uri, {
+                    start: { line: index, character: m.input.indexOf(m[2]) },
+                    end: { line: index, character: m.input.indexOf(m[2]) + m[2].length }
+
+                });
+                let params: ParameterInformation[] = [];
+                if (m[2].trim().length > 0) {
+                    params = m[2].split(',').map((value) => ({ label: value.trim() }));
                 } else {
                     params = [];
                 }
@@ -291,6 +360,7 @@ export const parseSnippets = async (textDocument: TextDocument) => {
     parseCustomSnip(textDocument);
     parsefuncs(textDocument);
     parsefuncsWithoutPrefix(textDocument);
+    parsefuncsDefines(textDocument);
     parseWords(textDocument);
 };
 
