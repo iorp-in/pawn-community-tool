2fbf8d41cb2f500d17d3edc9a44e7ecb59ba7ea2
IORP <indianoceanroleplay@gmail.com>
2021-03-06 17:08:47 +0530

    update

--- src/client/FoldingProvider.ts
+++ src/client/FoldingProvider.ts
@@ -17,11 +17,10 @@ function findFoldRanges(document: TextDocument) {
     let cCustomRegionCode = 0;
     let bracketRegion = 0;
     let bracketRegionCollect: number[] = [];
-    let lastLineBlank = false;
     let foldLineStart: number | null = null;
     for (var i = 0; i < document.lineCount; i++) {
         let line = document.lineAt(i).text;
-        if (i === 0 && line.length > 0) lastLineBlank = true, foldLineStart = i;
+        if (i === 0 && line.length > 0) foldLineStart = i;
         if (line.length > 0) {
             for (let index = 0; index < line.length; index++) {
                 const element = line[index];
@@ -33,20 +32,19 @@ function findFoldRanges(document: TextDocument) {
                     bracketRegion--;
                     const startN = bracketRegionCollect.pop();
                     if (startN !== undefined) {
-                        if (startN < i) {
-                            folds.push(new FoldingRange(startN, i));
+                        if (startN < i && startN - 1 !== i) {
+                            folds.push(new FoldingRange(startN, i - 1));
                         }
                     }
                 }
             }
         }
         if (bracketRegion === 0) {
-            if (!line) {
-                if (foldLineStart !== null && cCustomRegionCode === 0) {
+            if (!line && cCustomRegionCode === 0) {
+                if (foldLineStart !== null) {
                     folds.push(new FoldingRange(foldLineStart, i - 1));
                     foldLineStart = null;
                 }
-                lastLineBlank = true;
                 continue;
             } else {
                 if (foldLineStart !== null && ((cCustomRegionCode === 1 && end.test(line)) || (cCustomRegionCode === 2 && endComment.test(line)))) {
@@ -54,14 +52,12 @@ function findFoldRanges(document: TextDocument) {
                     foldLineStart = null;
                     cCustomRegionCode = 0;
                 }
-                if (lastLineBlank && foldLineStart === null) {
+                if (foldLineStart === null) {
                     if (line.length > 0) {
-                        if (cCustomRegionCode === 0 && start.test(line)) cCustomRegionCode = 1;
-                        if (cCustomRegionCode === 0 && startComment.test(line) && !endComment.test(line)) cCustomRegionCode = 2;
-                        if (!RegExp(/^\/\/(.*)/gm).test(line)) foldLineStart = i;
+                        if (cCustomRegionCode === 0 && start.test(line)) cCustomRegionCode = 1, foldLineStart = i;
+                        else if (cCustomRegionCode === 0 && startComment.test(line) && !endComment.test(line)) cCustomRegionCode = 2, foldLineStart = i;
                     }
                 }
-                lastLineBlank = false;
             }
         }
     }
