a16e72c9173f01a55758d9456ecd7ce79e40d4c6
IORP <indianoceanroleplay@gmail.com>
2021-03-06 18:27:10 +0530

    update

--- src/client/FoldingProvider.ts
+++ src/client/FoldingProvider.ts
@@ -10,6 +10,8 @@ export default class PawnFoldingProvider implements vscode.FoldingRangeProvider 
 function findFoldRanges(document: TextDocument) {
     const startRegEx = vscode.workspace.getConfiguration().get('pawn.language.startRegion') as string | null;
     const endRegEx = vscode.workspace.getConfiguration().get('pawn.language.endRegion') as string | null;
+    let showLastLineOfRegion = vscode.workspace.getConfiguration().get('pawn.language.showLastLineOfRegion') as true | false | null;
+    if (showLastLineOfRegion === null) showLastLineOfRegion = true;
 
     const start = RegExp(startRegEx !== null ? startRegEx : /(\/\/#region)|(\/\*)|\{/gm); // code 1
     const end = RegExp(endRegEx !== null ? endRegEx : /(\/\/#endregion)|(\*\/)|\}/gm); // code 1
@@ -25,7 +27,7 @@ function findFoldRanges(document: TextDocument) {
             const startN = customRegionCollect.pop();
             if (startN !== undefined) {
                 if (startN < i && startN - 1 !== i) {
-                    folds.push(new FoldingRange(startN, i - 1));
+                    folds.push(new FoldingRange(startN, showLastLineOfRegion ? i - 1 : i));
                 }
             }
         }
